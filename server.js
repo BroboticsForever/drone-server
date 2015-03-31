b50d6f80fbc38c5f794fece66976e9b8d1bd82ed
Jphsx <justin.anguiano1@gmail.com>
2015-03-31 14:48:08 -0500

    broswer to browser streaming with peer.js API ontop of WebRTC API

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
+//var net = require('net');
+
+//var app = express();
+//var httpServer = http.createServer(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var express = require('express');
-var child = require('child_process');
+//var child = require('child_process');
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,58 +26,23 @@ io.on('connection', function(socket){
  	 });
 
 	socket.on('setvideo', function(data){
-	/*if(data=='/dev/video0'){
-        var cmd = 'gst-launch-0.10';
-        var options = null;
-        var args =
-            ['v4l2src',
-                '!', 'video/x-raw-rgb,framerate=30/1',
-                '!', 'ffmpegcolorspace',
-                '!', 'vp8enc', 'speed=2',
-                '!', 'queue2',
-                '!', 'm.', 'webmmux', 'name=m', 'streamable=true',
-                '!', 'tcpserversink', 'host=localhost',
-                    'port=3001'];
-        var gstMuxer = child.spawn(cmd, args, options);*/
-
-       // gstMuxer.stderr.on('data', onSpawnError);
-       // gstMuxer.on('exit', onSpawnExit);
-
-    //}
-    // else{gstMuxer.kill();}
-
-	console.log('VideoSelected: ' + data);
+	    console.log('VideoSelected: ' + data);
   	});
 
 	socket.on('setctrl', function(data){
-	console.log('Controller Selected: ' + data);
+	    console.log('Controller Selected: ' + data);
 	});
 	
 	socket.on('vtest', function(data){
-	console.log('output path: ' +data);
+	    console.log('output path: ' +data);
 	});
 });
 
-http.listen(3000, function(){
+http.listen(3000,"0.0.0.0", function(){
   console.log('listening on *:3000');
 });
 
-var cmd = 'gst-launch-0.10';
-var options = null;
-var args1 = //vp8enc
-    ['v4l2src',
-        '!', 'video/x-raw-rgb,framerate=30/1',
-        '!', 'ffmpegcolorspace',
-        '!', 'vp8enc', 'speed=2',
-        '!', 'queue2',
-        '!', 'm.', 'webmmux', 'name=m', 'streamable=true',
-        '!', 'tcpserversink', 'host=localhost',
-        'port=3001'];
-var args2 = //theora enc
-    ['v4l2src',// device=/dev/video0',
-        '!', 'video/x-raw-yuv, width=500, height=300,framerate=30/1',
-        '!', 'theoraenc',
-        '!', 'oggmux',
-        '!', 'tcpserversink',
-        'port=3001'];
-var gstMuxer = child.spawn(cmd, args2, options);
+
+
+
+
