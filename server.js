0f75b04c0aac4ab08314ee096c4fd2009477fa42
Bryan-Burkett <crazydude008@gmail.com>
2015-04-29 13:34:37 -0500

    Update server.js
    Combined opencv and server streaming

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var http = require('http').Server(app);
 
 //var app = express();
 //var httpServer = http.createServer(app);
+var cv = require('opencv');
 var io = require('socket.io')(http);
 var path = require('path');
 var express = require('express');
@@ -36,13 +37,26 @@ io.on('connection', function(socket){
 	socket.on('vtest', function(data){
 	    console.log('output path: ' +data);
 	});
+
+    socket.on('frame', function(data) {
+        var _ref;
+        if (typeof data !== 'string') {
+            return;
+        }
+        data = data != null ? (_ref = data.split(',')) != null ? _ref[1] : void 0 : void 0;
+        return cv.readImage(new Buffer(data, 'base64'), function (err, im) {
+            return im.detectObject("./node_modules/opencv/data/haarcascade_frontalface_alt2.xml", {}, function (err, objects) {
+               // console.log('got to cv');
+                if (!((objects != null ? objects.length : void 0) > 0)) {
+                    return socket.emit('objects', []);
+                }
+                return socket.volatile.emit('objects', objects);
+
+            })
+        });
+    });
 });
 
 http.listen(3000,"0.0.0.0", function(){
   console.log('listening on *:3000');
 });
-
-
-
-
-
